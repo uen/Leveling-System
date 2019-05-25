d69f651e6bc7b3168502a670fd916ffcae4fa145
chesiren <44122832+chesiren@users.noreply.github.com>
2019-05-25 21:24:46 +0200

    added books

--- lua/entities/vrondakis_book/init.lua
+++ lua/entities/vrondakis_book/init.lua
@@ -1,22 +1,35 @@
-AddCSLuaFile("cl_init.lua")
-AddCSLuaFile("shared.lua")
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
 include("shared.lua")
-
-local PrintMore
+ 
 function ENT:Initialize()
-	self:SetModel("models/props_lab/bindergraylabel01a.mdl")
-	self:PhysicsInit(SOLID_VPHYSICS)
-	self:SetMoveType(MOVETYPE_VPHYSICS)
-	self:SetSolid(SOLID_VPHYSICS)
-	local phys = self:GetPhysicsObject()
-	phys:Wake()
-	self.GiveXPAmount = self.DarkRPItem.GiveXPAmountOnUse
+	self:SetModel(self.DarkRPItem.model)
+	self:SetColor(self.DarkRPItem.vrondakisColor)
+	
+    self:PhysicsInit( SOLID_VPHYSICS ) 
+    self:SetMoveType( MOVETYPE_VPHYSICS )   
+    self:SetSolid( SOLID_VPHYSICS ) 
+    local phys = self:GetPhysicsObject()
+        if (phys:IsValid()) then
+            phys:Wake()
+        end
+    end
 
+function ENT:Use( activator, caller )
+	local xp = self.DarkRPItem.xp
+	local bookname = self.DarkRPItem.vrondakisName
+    activator:addXP( xp, true)
+	DarkRP.notify(activator, 0,4,LevelSystemConfiguration.LangBookNotify)
+	self:Remove()
 end
 
-function ENT:Use(activator,caller)
-	if(activator:IsPlayer()) then
-		activator:addXP((self.DarkRPItem.GiveXPAmountOnUse) or 0)
+function ENT:Touch(entity)
+	if LevelSystemConfiguration.BookOnTouch then
+		if (!entity:IsPlayer()) then return end
+		local xp = self.DarkRPItem.xp
+		local bookname = self.DarkRPItem.vrondakisName
+		entity:addXP( xp, true)
+		DarkRP.notify(entity, 0,4,LevelSystemConfiguration.LangBookNotify)
+		self:Remove()
 	end
-	self:Remove()
 end
